bef56f39344d5e6a70cc65fb2d0bd15cedd34d8a
sv-kisel <svetlana.kisel+projects@gmail.com>
2025-10-31 11:00:57 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
   ['list'],
-  ['json', {  outputFile: 'results.json' }],
+  ['json', {  outputFile: 'playwright-report/results.json' }],
   ['junit', { outputFile: 'results.xml' }],
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
